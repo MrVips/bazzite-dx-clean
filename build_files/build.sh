ca36f35a578f883c5d5aadd1d6fa9c7f2a5c5557
MrVips <3705545+MrVips@users.noreply.github.com>
2026-08-08 11:17:55 +0200

    fixing and trixing

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,12 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
+
+### Remove packages
+sudo dnf remove \
+    waydroid \
+    waydroid-selinux
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -13,7 +19,18 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+
+dnf5 install -y \
+    google-noto-sans-fonts \
+    podman-tui \
+    restic \
+    zsh \
+    qemu \
+    libvirt \
+    qemu-kvm \
+    virt-manager \
+    edk2-ovmf \
+    guestfs-tools
 
 # Use a COPR Example:
 #
@@ -22,6 +39,28 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
+dnf5 config-manager addrepo --from-repofile="https://packages.microsoft.com/yumrepos/vscode/config.repo"
+dnf5 config-manager setopt vscode-yum.enabled=0
+dnf5 install --enable-repo="vscode-yum" -y \
+    code
 
-systemctl enable podman.socket
+dnf5 config-manager addrepo --from-repofile="https://repository.mullvad.net/rpm/stable/mullvad.repo"
+dnf5 config-manager setopt vscode-yum.enabled=0
+dnf5 install --enable-repo="mullvad-stable" -y\
+    mullvad-vpn
+
+# Enable services
+systemctl --global enable ublue-user-setup.service
+systemctl enable bazzite-dx-groups.service
+
+# Clean package manager cache
+dnf5 clean all
+
+# Clean temporary files
+rm -rf /tmp/* || true
+
+# Cleanup the entirety of `/var`.
+# None of these get in the end-user system and bootc lints get super mad if anything is in there
+rm -rf /var
+mkdir -p /var/tmp
+chmod -R 1777 /var/tmp
